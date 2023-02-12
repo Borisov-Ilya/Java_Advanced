1343919b1eaea0c006ded3b05e4ea68324c0b201
Borisov-Ilya <borisov.ilya22@gmail.com>
2023-02-12 21:51:52 +0300

    Задание №3 - удалил лишние методы и поля

--- src/main/java/Task3/sweetGift/Candy.java
+++ src/main/java/Task3/sweetGift/Candy.java
@@ -1,33 +1,7 @@
 package Task3.sweetGift;
 
 public class Candy extends Sweets {
-    String name;
-    int price;
-    int weight;
-    String uniqueParameter;
-
     public Candy(String name, int price, int weight, String uniqueParameter){
         super(name, price, weight, uniqueParameter);
-        this.name = name;
-        this.price = price;
-        this.weight =  weight;
-        this.uniqueParameter = uniqueParameter;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public String getUniqueParameter() {
-        return uniqueParameter;
-    }
-
 }
